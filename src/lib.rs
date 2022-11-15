16e9177b778a45862e24b620e26c5c32cf35345d
jjminsuh <minsuh3203@gmail.com>
2022-11-15 20:16:46 +0900

    refact: port 임의 설정

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,17 @@
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", &name)
-// }
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
   let server = HttpServer::new( || {
     App::new()
         .route("/health_check", web::get().to(health_check))
-        // .route("/", web::get().to(greet))
-        // .route("/{name}", web::get().to(greet))
   })
-  .bind("127.0.0.1:8000")?
+  .listen(listener)?
   .run();
 
   Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,21 @@
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+  let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+  let port = listener.local_addr().unwrap().port();
+  let server = gdsc_rust_study::run(listener).expect("Failed tp bind address");
+  let _ = tokio::spawn(server);
+
+  format!("http://127.0.0.1:{port}")
+}
+
 #[tokio::test]
 async fn health_check_works() {
-  spawn_app();
-
+  let address = spawn_app();
   let client = reqwest::Client::new();
 
   let response = client
-    .get("http://127.0.0.1:8000/health_check")
+    .get(&format!("{}/health_check", &address))
     .send()
     .await
     .expect("Failed to execute request.");
@@ -14,8 +24,4 @@ async fn health_check_works() {
   assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-  let server = gdsc_rust_study::run().expect("Failed tp bind address");
-  
-  let _ = tokio::spawn(server);
-}
+
