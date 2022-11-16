742fe444d0498c9ffc4c1c34d36a00430768e643
Minsuh Jo <78486898+jjminsuh@users.noreply.github.com>
2022-11-16 15:53:45 +0900

    Week 5 (#4)
    * init
    
    * rust backend example
    
    * HttpResponse OK 연습
    
    * testing example
    
    * refact: port 임의 설정
    
    * add: 들어온 data field valid, invalid test
    
    * add: shell script

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", &name)
-// }
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+#[derive(serde::Deserialize)]
+struct FormData {
+  email: String,
+  name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+  HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
   let server = HttpServer::new( || {
     App::new()
         .route("/health_check", web::get().to(health_check))
-        // .route("/", web::get().to(greet))
-        // .route("/{name}", web::get().to(greet))
-  })
-  .bind("127.0.0.1:8000")?
-  .run();
+        .route("/subscriptions", web::post().to(subscribe))
+      })
+    .listen(listener)?
+    .run();
 
   Ok(server)
 }
