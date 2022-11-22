e5b448912f8ff63a19f8487016fe0511dc60de75
jjminsuh <minsuh3203@gmail.com>
2022-11-22 22:12:18 +0900

    add: week 6 DB 기초 작업 work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::net::TcpListener;
-use gdsc_rust_study::run;
+use gdsc_rust_study::{startup::run, configuration::get_configuration};
+use sqlx::{PgConnection, Connection, PgPool};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed do connect to Postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
