c70047c679d4930dd9ae7939e8b388cc67f000a3
jjminsuh <minsuh3203@gmail.com>
2023-02-07 18:12:43 +0900

    test 추가

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,6 +4,12 @@ use uuid::Uuid;
 use gdsc_rust_study::configuration::{get_configuration, DatabaseSettings};
 use gdsc_rust_study::startup::run;
 use gdsc_rust_study::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let subscriber  = get_subscriber("test".into(), "debug".into());
+    init_subscriber(subscriber);
+});
 
 pub struct TestApp {
     pub address: String,
@@ -11,6 +17,8 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
